e0ca5d97a10b930e9d81b7b666709a729fc011f4
C-D-Lewis <Bonsitm@gmail.com>
2020-02-20 22:27:58 +0000

    Begin using commands

--- src/index.js
+++ src/index.js
@@ -8,14 +8,7 @@ const CONFIG_SCHEMA = {
   properties: {
     resources: {
       type: 'array',
-      items: {
-        type: 'object',
-        required: ['name', 'path'],
-        properties: {
-          name: { type: 'string' },
-          path: { type: 'string' },
-        },
-      },
+      items: { type: 'string' },
     },
     authorization: {
       type: 'object',
@@ -34,7 +27,7 @@ const CONFIG_SCHEMA = {
 };
 
 const ARGS = process.argv.slice(2);
-const loadedConfig = {};
+const commands = [];
 
 const validateConfig = (name, config) => {
   const results = validate(config, CONFIG_SCHEMA);
@@ -46,18 +39,26 @@ const validateConfig = (name, config) => {
 const loadConfigs = () => {
   const configs = readdirSync(`${__dirname}/../../`)
     .filter(p => p.includes(CONFIG_PREFIX))
-    .map((p) => ({ name: p, data: require(`${__dirname}/../../${p}`) }));
-  configs.forEach((p) => {
-    validateConfig(p.name, p.data);
+    .map(p => ({ name: p, data: require(`${__dirname}/../../${p}`) }));
+  configs.forEach((config) => {
+    validateConfig(config.name, config.data);
 
-    // Add resources
-    
+    config.data.resources.forEach(resource => commands.push(resource));
   });
 };
+
+const handleArgs = () => {
+  // Find a loaded command that matches args
+  const found = commands.find((command) => {
+    const parts = command.split('/').filter(p => p.length);
+    
+  })
+}
 
 const main = () => {
   try {
     loadConfigs();
+    handleArgs();
   } catch (e) {
     console.log(e);
   }
